a0e2fd858fc7e3cf45f41cc3461d2f8b16adf84e
vdp11 <145059840+vdp11@users.noreply.github.com>
2023-10-03 00:02:37 +0300

    Update watch_next.py

--- watch/watch_next.py
+++ watch/watch_next.py
@@ -1,3 +1,5 @@
+import spacy
+
 # Read movie descriptions from movies.txt and store them in a list
 def read_movie_descriptions():
     movie_descriptions = []
@@ -6,10 +8,8 @@ def read_movie_descriptions():
             movie_descriptions.append(line.strip())
     return movie_descriptions
 
-import spacy
-
-# Load the spaCy language model (you can change the model to en_core_web_md by replacing en_core_web_sm)
-nlp = spacy.load("en_core_web_sm")
+# Load the spaCy language model (you can change the model to en_core_web_sm by replacing en_core_web_md)
+nlp = spacy.load("en_core_web_md")
 
 # Calculate similarity scores between the given description and all other movie descriptions
 def calculate_similarity_scores(target_description, movie_descriptions):
@@ -23,6 +23,9 @@ def calculate_similarity_scores(target_description, movie_descriptions):
 
 # Find the title of the most similar movie
 def recommend_movie(target_description, movie_descriptions, movie_titles):
+
+    target_doc = nlp(target_description)
+
     similarity_scores = calculate_similarity_scores(target_description, movie_descriptions)
     
     if not similarity_scores:
@@ -34,7 +37,7 @@ def recommend_movie(target_description, movie_descriptions, movie_titles):
 if __name__ == "__main__":
     movie_descriptions = read_movie_descriptions()
     
-    # Assuming you have a list of movie titles in the same order as the descriptions
+
     movie_titles = ["Movie A", "Movie B", "Movie C", "Movie D", "Movie E", "Movie F", "Movie G", "Movie H", "Movie I", "Movie J"]  # Replace with your movie titles
     
     if len(movie_descriptions) != len(movie_titles):
@@ -42,7 +45,7 @@ if __name__ == "__main__":
     else:
         target_description = "Will he save their world or destroy it? When the Hulk becomes too dangerous for the Earth, the Illuminati trick Hulk into a shuttle and launch him into space to a planet where the Hulk can live in peace. Unfortunately, Hulk lands on the planet Sakaar where he is sold into slavery and trained as a gladiator."
         
+        target_doc = nlp(target_description)
+        
         recommended_movie = recommend_movie(target_description, movie_descriptions, movie_titles)
         print(f"Recommended Movie: {recommended_movie}")
-
-
